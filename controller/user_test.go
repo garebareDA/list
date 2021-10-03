2dad36cc9dae424a7f9dfd462e8d6605ed086e64
garebareDA <garedaiki@gmail.com>
2021-10-04 02:07:49 +0900

    fix:controller test

--- controller/user_test.go
+++ controller/user_test.go
@@ -1,28 +1,104 @@
 package controller_test
 
 import (
+	"io"
+	"net"
+	"net/http"
+	"net/url"
 	"testing"
 
 	"github.com/garebareDA/list/controller"
 	"github.com/garebareDA/list/log"
 
-	"github.com/zhouhui8915/go-socket.io-client"
 	socketio "github.com/googollee/go-socket.io"
 )
 
-func UserTest_Message(t *testing.T) {
-	logger := log.New()
-	s := socketio.NewServer(nil)
-	userCtrl := controller.NewUserController(s, logger);
+type fakeNamespace interface {
+	Context() interface{}
+	SetContext(ctx interface{})
 
-	s.OnEvent("/", "message", userCtrl.Message)
-	
+	Namespace() string
+	Emit(eventName string, v ...interface{})
+
+	Join(room string)
+	Leave(room string)
+	LeaveAll()
+	Rooms() []string
 }
 
-func UserTest_Join(t *testing.T) {
+type fakeConn struct {
+	io.Closer
+	fakeNamespace
+	IDFunc           func() string
+	URLFunc          func() url.URL
+	LocalAddrFunc    func() net.Addr
+	RemoteAddrFunc   func() net.Addr
+	RemoteHeaderFunc func() http.Header
+	JoinFunc func(room string)
+}
+
+func (c fakeConn) ID() string {
+	return c.IDFunc()
+}
+
+func (c fakeConn) URL() url.URL {
+	return c.URLFunc()
+}
+
+func (c fakeConn) LocalAddr() net.Addr {
+	return c.LocalAddrFunc()
+}
+
+func (c fakeConn) RemoteAddr() net.Addr {
+	return c.RemoteAddrFunc()
+}
+
+func (c fakeConn) RemoteHeader() http.Header {
+	return c.RemoteHeaderFunc()
+}
+
+func (c fakeConn) Join(msg string) {
+	c.JoinFunc(msg)
+}
+
+func TestUser_Message(t *testing.T) {
 	logger := log.New()
 	s := socketio.NewServer(nil)
-	userCtrl := controller.NewUserController(s, logger);
+	userCtrl := controller.NewUserController(s, logger)
 
-	s.OnEvent("/", "join", userCtrl.Join)
+	req := `{
+		"id": "id",
+		"name": "name",
+		"icon": "icon",
+		"room": "room",
+		"message": "message"
+		}`
+	conn := &fakeConn{}
+	userCtrl.Message(conn, req)
+}
+
+func TestUser_Join(t *testing.T) {
+	logger := log.New()
+	s := socketio.NewServer(nil)
+	userCtrl := controller.NewUserController(s, logger)
+	req := `{
+		"id": "id",
+		"name": "name",
+		"icon": "icon",
+		"room": "room",
+		"message": "message"
+		}`
+
+	conn := fakeConn{
+		JoinFunc: func(room string) {
+			if room != "room" {
+				logger.Fatalf("failde room join %s", room)
+			}
+		},
+
+		IDFunc: func() string {
+			return "1"
+		},
+	}
+	userCtrl.Join(conn, req)
 }
--- go.mod
+++ go.mod
@@ -12,21 +12,25 @@ require (
 )
 
 require (
+	github.com/davecgh/go-spew v1.1.1 // indirect
 	github.com/gin-contrib/sse v0.1.0 // indirect
 	github.com/go-playground/locales v0.13.0 // indirect
 	github.com/go-playground/universal-translator v0.17.0 // indirect
 	github.com/go-playground/validator/v10 v10.4.1 // indirect
 	github.com/gofrs/uuid v4.0.0+incompatible // indirect
 	github.com/golang/protobuf v1.3.3 // indirect
-	github.com/gomodule/redigo v1.8.4 // indirect
+	github.com/gomodule/redigo v1.8.5 // indirect
 	github.com/google/go-cmp v0.5.6 // indirect
 	github.com/gorilla/websocket v1.4.2 // indirect
+	github.com/graarh/golang-socketio v0.0.0-20170510162725-2c44953b9b5f // indirect
 	github.com/json-iterator/go v1.1.9 // indirect
 	github.com/leodido/go-urn v1.2.0 // indirect
 	github.com/mattn/go-colorable v0.1.2 // indirect
 	github.com/mattn/go-isatty v0.0.12 // indirect
 	github.com/modern-go/concurrent v0.0.0-20180228061459-e0a39a4cb421 // indirect
 	github.com/modern-go/reflect2 v0.0.0-20180701023420-4b7aa43c6742 // indirect
+	github.com/pmezard/go-difflib v1.0.0 // indirect
+	github.com/stretchr/testify v1.7.0 // indirect
 	github.com/ugorji/go/codec v1.1.7 // indirect
 	github.com/valyala/bytebufferpool v1.0.0 // indirect
 	github.com/valyala/fasttemplate v1.0.1 // indirect
@@ -36,4 +40,5 @@ require (
 	golang.org/x/sys v0.0.0-20200116001909-b77594299b42 // indirect
 	golang.org/x/xerrors v0.0.0-20191204190536-9bdfabe68543 // indirect
 	gopkg.in/yaml.v2 v2.2.8 // indirect
+	gopkg.in/yaml.v3 v3.0.0-20200313102051-9f266ea9e77c // indirect
 )
