7e3aa6c4da08f6a80fed78c7dcd9cdf34d9eab71
garebareDA <garedaiki@gmail.com>
2021-10-07 02:17:49 +0900

    fix test

--- frontend/src/__test__/routes/Group.test.tsx
+++ frontend/src/__test__/routes/Group.test.tsx
@@ -1,8 +1,14 @@
 import { render, fireEvent } from '@testing-library/react';
+import React from 'react';
 import ReactDOM from 'react-dom'
 import fetchMock, { MockResponseInit } from 'jest-fetch-mock';
 import { act, create, ReactTestRenderer } from 'react-test-renderer';
 import Group from '../../routes/Group';
+
+jest.mock('react', () => ({
+    ...jest.requireActual('react'),
+    useEffect: jest.fn(),
+}));
 
 jest.mock('react-router-dom', () => ({
     ...jest.requireActual('react-router-dom'),
